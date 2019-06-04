fd143e739e576b2a216c2bef106dd806d7a07c37
Brad Garropy <bradgarropy@gmail.com>
2019-06-04 14:12:32 -0500

    add firebase.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,30 @@
 import React from "react"
+import {useState, useEffect} from "react"
 import Layout from "../components/Layout"
 import Meta from "../components/SEO/Meta"
 import Facebook from "../components/SEO/Facebook"
 import Twitter from "../components/SEO/Twitter"
+import {firestore} from "../utils/firebase"
 
 const Index = () => {
+    const [users, setUsers] = useState([])
+
+    const getUsers = async() => {
+        const query = await firestore.collection("users").get()
+        const users = query.docs.map(user => {
+            return {
+                id: user.id,
+                ...user.data(),
+            }
+        })
+
+        setUsers(users)
+    }
+
+    useEffect(() => {
+        getUsers()
+    }, [])
+
     return (
         <Layout>
             <Meta title="gatsby starter"/>
@@ -12,6 +32,11 @@ const Index = () => {
             <Twitter/>
 
             <h1>home</h1>
+            {users.map(user => (
+                <p key={user.id}>
+                    {user.firstName} {user.lastName}
+                </p>
+            ))}
         </Layout>
     )
 }
