70fddc930abd82c8182b711d83308d7e1bf1bcfa
Brad Garropy <bradgarropy@gmail.com>
2019-06-13 23:42:43 -0500

    group page.

--- pages/group/index.js
+++ pages/group/index.js
@@ -1,10 +1,26 @@
 import React from "react"
+import {useState, useEffect} from "react"
+import {withRouter} from "next/router"
 import Layout from "../../components/Layout"
 import Meta from "../../components/SEO/Meta"
 import Facebook from "../../components/SEO/Facebook"
 import Twitter from "../../components/SEO/Twitter"
+import {firestore} from "../../utils/firebase"
 
-const groupPage = () => {
+const GroupPage = props => {
+    const {id} = props.router.query
+    const [group, setGroup] = useState()
+
+    useEffect(() => {
+        const getUsers = async() => {
+            const query = await firestore.doc(`/groups/${id}`).get()
+            const group = await query.data()
+            setGroup(group)
+        }
+
+        getUsers()
+    }, [id])
+
     return (
         <Layout>
             <Meta title="group"/>
@@ -12,8 +28,25 @@ const groupPage = () => {
             <Twitter/>
 
             <h1>Group</h1>
+            {group && (
+                <>
+                    <h3>Name</h3>
+                    <p>{group.name}</p>
+
+                    <h3>Members</h3>
+                    {group.members.map((member, index) => (
+                        <p key={index}>{member}</p>
+                    ))}
+
+                    <h3>Date</h3>
+                    <p>{group.date}</p>
+
+                    <h3>Limit</h3>
+                    <p>{group.limit}</p>
+                </>
+            )}
         </Layout>
     )
 }
 
-export default groupPage
+export default withRouter(GroupPage)
