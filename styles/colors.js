d82920faebb05c46b4a52286edf85c0fc074ff8c
Brad Garropy <bradgarropy@gmail.com>
2019-06-17 23:44:21 -0500

    custom colors.

--- styles/colors.js
+++ styles/colors.js
@@ -1,8 +1,11 @@
 const colors = {
-    darkPurple: "#663399",
-    lightPurple: "#ccb3e6",
     white: "#ffffff",
     black: "#000000",
+    lightRed: "#f5624d",
+    red: "#cc231e",
+    green: "#34a65f",
+    darkGreen: "#0f8a5f",
+    blue: "#235e6f",
 }
 
 export default colors
