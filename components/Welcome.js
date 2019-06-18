240917ab09684ea61f37a68ef20da781c2a53726
Brad Garropy <bradgarropy@gmail.com>
2019-06-17 23:45:48 -0500

    simple welcome message.

--- components/Welcome.js
+++ components/Welcome.js
@@ -1,12 +1,12 @@
 import React from "react"
 import {useContext} from "react"
-import {UserContext} from "../context/user"
+import {UserContext} from "../context"
 
 const Welcome = () => {
     const userContext = useContext(UserContext)
     const {user} = userContext
 
-    return <h3>Welcome, {user.email}</h3>
+    return <p>{user.email}</p>
 }
 
 export default Welcome
