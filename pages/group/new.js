f4eaa3439ced9146114831fbc817a7c8bbeac9c9
Brad Garropy <bradgarropy@gmail.com>
2019-07-18 14:33:23 -0500

    import bug fix.

--- pages/group/new.js
+++ pages/group/new.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../../components/Layout"
-import {Meta, Facebook, Twitter} from "../components/SEO"
+import {Meta, Facebook, Twitter} from "../../components/SEO"
 import NewGroup from "../../components/NewGroup"
 
 const NewGroupPage = () => {
